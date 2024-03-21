4abd683f04c9e5dc49da38110b2a625d7dc4b46b
HeinrickVHO2 <victor.henrick9@gmail.com>
2024-03-21 03:22:53 -0300

    Alterações na Dashboard

---
+++ site/src/database/script-tabelas.sql
@@ -0,0 +1,14 @@
+
+CREATE DATABASE HowToPLay;
+
+USE HowToPlay;
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	CPF VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+
